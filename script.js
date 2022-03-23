4246ba5193e065a4af6f57229184c1085a9f0c34
AndrewDorazio <adorazio1997@gmail.com>
2022-03-23 14:50:18 -0400

    Link to javascript, create click function for profiles button

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 // Not sure what Im adding here yet
 
 function buttonFunction(){
-    let linkedInButton = document.getElementById("buttonAppear").innerHTML = '<button onclick="secondFunction()">Some text here</button>';
+    alert('omg');
 }
