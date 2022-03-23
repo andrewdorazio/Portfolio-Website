35eabee4353478329b4032b971e5af54f5d85f45
AndrewDorazio <adorazio1997@gmail.com>
2022-03-23 13:50:05 -0400

    Add JS function to get social media button elements

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // Javascript file
 // Not sure what Im adding here yet
+
+function buttonFunction(){
+    let linkedInButton = document.getElementById("buttonAppear").innerHTML = '<button onclick="secondFunction()">Some text here</button>';
+}
